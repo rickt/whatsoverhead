80be2962c73151a3dad7abdc757aad4aea9849a0
rick tait <rickt@rickt.org>
2024-10-23 18:14:05 -0700

    updated to use GCP_SERVICE_ACCOUNT for better security

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,7 +2,7 @@
 
 . ./.env
 
-echo "running: gcloud beta deploy ${ENDPOINT} --region ${CLOUDRUN_REGION} --image gcr.io/${GCP_PROJECT_ID}/${ENDPOINT} --port ${PORT} --cpu ${CLOUDRUN_CPU} --memory ${CLOUDRUN_MEMORY}Gi --max-instances ${CLOUDRUN_MAXINSTANCES} --concurrency ${CLOUDRUN_CONCURRENCY} --allow-unauthenticated"
+echo "running: gcloud beta deploy ${ENDPOINT} --region ${CLOUDRUN_REGION} --image gcr.io/${GCP_PROJECT_ID}/${ENDPOINT} --port ${PORT} --cpu ${CLOUDRUN_CPU} --memory ${CLOUDRUN_MEMORY}Gi --max-instances ${CLOUDRUN_MAXINSTANCES} --concurrency ${CLOUDRUN_CONCURRENCY} --service-account ${GCP_SERVICE_ACCOUNT} --allow-unauthenticated"
 echo ""
 
 gcloud beta run deploy ${ENDPOINT} \
@@ -13,6 +13,7 @@ gcloud beta run deploy ${ENDPOINT} \
     --memory ${CLOUDRUN_MEMORY}Gi \
     --max-instances ${CLOUDRUN_MAXINSTANCES} \
     --concurrency ${CLOUDRUN_CONCURRENCY} \
+	 --service-account ${GCP_SERVICE_ACCOUNT} \
     --allow-unauthenticated
 
 # EOF
